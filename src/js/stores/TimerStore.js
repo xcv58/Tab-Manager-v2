d1a4c64e2d9ddc9db2816da32ed1120e2eb0fa9a
xcv58 <i@xcv58.com>
2017-09-02 21:38:15 -0400

    Update style

--- src/js/stores/TimerStore.js
+++ src/js/stores/TimerStore.js
@@ -2,15 +2,19 @@ import { action, observable } from 'mobx'
 
 class TimerStore {
   @observable timer = 0
-  @action resetTimer = () => {
+  @action
+  resetTimer = () => {
     this.timer = 0
   }
 }
 
 const timerStore = new TimerStore()
 
-setInterval(action(() => {
-  timerStore.timer += 1
-}), 1000)
+setInterval(
+  action(() => {
+    timerStore.timer += 1
+  }),
+  1000
+)
 
 export default timerStore
