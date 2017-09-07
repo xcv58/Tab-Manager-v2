19ae65f887e6d8b9aef9d3757614db2346a77e29
xcv58 <i@xcv58.com>
2017-09-06 23:59:46 -0700

    Add title for DragPreview

--- src/js/components/DragPreview.js
+++ src/js/components/DragPreview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import DraggableTab from './Tab/DraggableTab'
+import { blue } from 'material-ui/colors'
 
 @inject('tabStore')
 @observer
@@ -14,9 +15,15 @@ export default class DragPreview extends React.Component {
       <div
         ref={setDragPreview}
         style={{
+          width: '64%',
           position: 'fixed',
+          background: blue[100],
+          textAlign: 'center',
           top: 2048
         }}>
+        <h3>
+          {sources.length} tab{sources.length > 1 && 's'}
+        </h3>
         {content}
       </div>
     )
