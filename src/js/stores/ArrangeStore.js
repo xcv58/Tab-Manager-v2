38c8a46baee9cd61571cf26ba9c3942a98f3ce92
xcv58 <i@xcv58.com>
2017-09-07 22:19:04 -0700

    Update sortTabs to group tabs by domain then sort in window

--- src/js/stores/ArrangeStore.js
+++ src/js/stores/ArrangeStore.js
@@ -1,38 +1,58 @@
 import { action, computed } from 'mobx'
 import { moveTabs, tabComparator } from '../libs'
 
+const urlPattern = /.*:\/\/[^/]*/
+const getDomain = (url) => {
+  const matches = url.match(urlPattern)
+  if (matches) {
+    return matches[0]
+  }
+  return url
+}
+
 export default class ArrangeStore {
   constructor (store) {
     this.store = store
   }
 
   @computed
-  get urlTabMap () {
+  get domainTabsMap () {
     return this.store.windowStore.tabs.reduce((acc, tab) => {
-      const { url } = tab
-      acc[url] = acc[url] || []
-      acc[url].push(tab)
+      const domain = getDomain(tab.url)
+      acc[domain] = acc[domain] || []
+      acc[domain].push(tab)
       return acc
     }, {})
   }
 
-  @computed
-  get duplicatedTabs () {
-    return Object.values(this.urlTabMap).filter(x => x.length > 1)
-  }
-
   @action
-  sortTabs = () => {
-    this.store.windowStore.windows.map((win) => {
-      const tabs = win.tabs.sort(tabComparator)
-      moveTabs(tabs, win.id, 0)
-    })
-    this.groupDuplicateTabs()
+  sortTabs = async () => {
+    await this.groupTabs()
+    await this.sortInWindow()
   }
 
-  groupDuplicateTabs = () => {
-    this.duplicatedTabs.map((tabs) => {
-      moveTabs(tabs, tabs[0].windowId, -1)
+  groupTabs = async () => {
+    await Promise.all(
+      Object.entries(this.domainTabsMap).map(
+        async ([ domain, tabs ]) => {
+          if (tabs.length > 1) {
+            const sortedTabs = tabs.sort(tabComparator)
+            const { windowId, pinned } = sortedTabs[0]
+            await moveTabs(
+              sortedTabs.map(x => ({ ...x, pinned })),
+              windowId
+            )
+          }
+        }
+      )
+    )
+  }
+
+  sortInWindow = async () => {
+    const windows = await chrome.windows.getAll({ populate: true })
+    windows.map((win) => {
+      const tabs = win.tabs.sort(tabComparator)
+      moveTabs(tabs, win.id)
     })
   }
 }
