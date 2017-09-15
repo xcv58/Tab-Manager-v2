73226f0b16dfe77a69d0000de9a43f3832e2bca3
xcv58 <i@xcv58.com>
2017-09-14 22:59:48 -0700

    Fix #23
    Add `expectedTabId` to check whether new active tab is triggered by system shortcut
    If yes, we add count and reset the `resetCountHandler` and increase count base on the condition

--- src/js/background/TabHistory.js
+++ src/js/background/TabHistory.js
@@ -16,6 +16,7 @@ export default class TabHistory {
   tabHistory = []
   count = 0
   resetCountHandler = null
+  expectedTabId = null
 
   resetCount = () => {
     if (this.resetCountHandler != null) {
@@ -30,6 +31,7 @@ export default class TabHistory {
         const lastTab = this.tabHistory[length - 1]
         this.tabHistory = [ ...untouchedTabs, ...touchedTabs, lastTab ]
         this.count = 0
+        this.resetCountHandler = null
       },
       1000
     )
@@ -55,19 +57,26 @@ export default class TabHistory {
     this.resetCount()
     const { length } = this.tabHistory
     if (length > 1) {
-      const index = length - 1 - this.count
-      if (index < 0) {
-        this.count = 0
-        const tabs = this.tabHistory.slice(0, length - 1).reverse()
-        this.tabHistory = [ ...tabs, this.tabHistory[length - 1] ]
-        return this.activateTab()
-      }
-      activateTab(this.tabHistory[index].tabId)
+      const index = Math.max(length - 1 - this.count, 0)
+      const { tabId } = this.tabHistory[index]
+      this.expectedTabId = tabId
+      activateTab(tabId)
     }
   }
 
   onActivated = async (activeInfo) => {
     const { tabId, windowId } = activeInfo
+    if (tabId !== this.expectedTabId) {
+      const { length } = this.tabHistory
+      if (this.resetCountHandler) {
+        this.resetCount()
+        const index = this.tabHistory.findIndex(x => x.tabId === tabId)
+        if (index < length - this.count) {
+          this.count += 1
+        }
+      }
+    }
+    this.expectedTabId = null
     const tab = await chrome.tabs.get(tabId)
     this.add({ tabId, windowId, title: tab.title })
   }
