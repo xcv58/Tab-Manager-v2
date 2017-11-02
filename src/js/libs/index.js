adeadee7bf92453c65fd63c4cac83571c83a3345
xcv58 <i@xcv58.com>
2017-11-01 21:24:51 -0700

    Revert windowComparator to support sort based on focused

--- src/js/libs/index.js
+++ src/js/libs/index.js
@@ -64,12 +64,12 @@ export const tabComparator = (a, b) => {
 }
 
 export const windowComparator = (a, b) => {
-  // if (a.lastFocused ^ b.lastFocused) {
-  //   return b.lastFocused ? 1 : -1
-  // }
-  // if (a.focused ^ b.focused) {
-  //   return b.focused ? 1 : -1
-  // }
+  if (a.lastFocused ^ b.lastFocused) {
+    return b.lastFocused ? 1 : -1
+  }
+  if (a.focused ^ b.focused) {
+    return b.focused ? 1 : -1
+  }
   if (a.tabs.length !== b.tabs.length) {
     return b.tabs.length - a.tabs.length
   }
