fa385495107994b3d5036ee5dbfd006e722f3729
xcv58 <i@xcv58.com>
2017-11-02 21:55:24 -0700

    Fix popup winodw jump issue
    Fix #59

--- src/js/components/WindowList.js
+++ src/js/components/WindowList.js
@@ -19,25 +19,11 @@ const View = (props) => {
 @inject('windowStore')
 @observer
 export default class WindowList extends React.Component {
-  resize = () => {
-    const bottom = Math.max(
-      ...Object.entries(this.refs)
-      .filter(([ key ]) => key !== 'shadowScrollbars')
-      .map(([ _, x ]) => x.getBoundingClientRect().bottom)
-    )
-    const height = `${Math.max(bottom, 300)}px`
-    document.getElementsByTagName('html')[0].style.height = height
-    document.body.style.height = height
-  }
-
-  componentDidUpdate = this.resize
-
   render () {
     const { windowStore: { windows, width } } = this.props
     const winList = windows.map((win) => (
       <Window
         key={win.id}
-        ref={win.id}
         win={win}
         width={width}
         getWindowList={() => this}
