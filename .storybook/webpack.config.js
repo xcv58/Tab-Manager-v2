809c13018ef337327c238a515602273f30d9920f
xcv58 <i@xcv58.com>
2017-11-14 21:50:14 -0800

    Fix yarn build-storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,5 +5,14 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const config = require('../webpack.config')
 
-module.exports = require('../webpack.config')
+module.exports = (storybookBaseConfig, configType) => {
+  if (configType === 'PRODUCTION') {
+    // see https://github.com/storybooks/storybook/issues/1570
+    storybookBaseConfig.plugins = storybookBaseConfig.plugins.filter(plugin => plugin.constructor.name !== 'UglifyJsPlugin')
+  }
+
+  storybookBaseConfig.module = config.module
+  return storybookBaseConfig
+}
