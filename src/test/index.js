d93f20805c8387d27bca20ab28a0ba3b77cea438
xcv58 <i@xcv58.com>
2017-11-18 13:19:30 -0800

    Use sinon-chrome

--- src/test/index.js
+++ src/test/index.js
@@ -1,7 +1,9 @@
 import { configure } from 'enzyme'
+import chrome from 'sinon-chrome/extensions'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+global.chrome = chrome
 
 export * from 'enzyme'
 export * from 'sinon'
