3643710d90ff832d50b79657a7e367e91d2fdffb
xcv58 <i@xcv58.com>
2017-12-17 16:42:29 -0800

    Simplify window sort algorithm

--- src/js/libs/index.js
+++ src/js/libs/index.js
@@ -109,12 +109,6 @@ export const tabComparator = (a, b) => {
 }
 
 export const windowComparator = (a, b) => {
-  if (a.lastFocused ^ b.lastFocused) {
-    return b.lastFocused ? 1 : -1
-  }
-  if (a.focused ^ b.focused) {
-    return b.focused ? 1 : -1
-  }
   if (a.alwaysOnTop !== b.alwaysOnTop) {
     return b.alwaysOnTop ? 1 : -1
   }
