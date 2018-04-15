c426b334cc99088f8dce272605c04338ed1573fa
xcv58 <i@xcv58.com>
2018-04-15 11:37:52 -0700

    Use controlled Tooltip

--- src/js/components/Tab/TabTooltip.js
+++ src/js/components/Tab/TabTooltip.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import Tooltip from 'material-ui/Tooltip'
 
 @observer
 export default class TabTooltip extends React.Component {
+  state = { open: false }
+
+  handleTooltipClose = () => {
+    this.setState({ open: false })
+  }
+
+  handleTooltipOpen = () => {
+    const { dragging } = this.props.dragStore
+    if (!dragging) {
+      this.setState({ open: true })
+    }
+  }
+
   render () {
     const { title, url } = this.props.tab
-    return (
-      <div style={{
-        whiteSpace: 'normal',
-        wordBreak: 'break-all',
-        wordWrap: 'break-word',
-        maxWidth: '20rem'
-      }}>
+    const tooltip = (
+      <div
+        style={{
+          whiteSpace: 'normal',
+          wordBreak: 'break-all',
+          wordWrap: 'break-word',
+          maxWidth: '20rem'
+        }}
+      >
         <p>{title}</p>
         <p>{url}</p>
       </div>
+    )
+    return (
+      <Tooltip
+        title={tooltip}
+        onClose={this.handleTooltipClose}
+        onOpen={this.handleTooltipOpen}
+        open={this.state.open}
+        enterDelay={300}
+        leaveDelay={300}
+      >
+        {this.props.children}
+      </Tooltip>
     )
   }
 }
