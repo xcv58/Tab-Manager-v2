2181819883e87d9e1a76937fe7664e087c5d5d09
Yihong <chenyihonglove@gmail.com>
2018-05-08 20:45:42 -0700

    fix: Fix horizontal navigation align issue (#170)

---
+++ src/js/components/Tab/__tests__/TabTooltip.test.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { shallow, expect, describe, it } from 'test'
+import IconButton from 'material-ui/IconButton'
+import Tooltip from 'material-ui/Tooltip'
+import Icon from '../Icon'
+import TabTooltip from 'components/Tab/TabTooltip'
+
+const props = {
+  children: 'children',
+  faked: false,
+  dragStore: { dragging: false },
+  hoverStore: { hovered: true },
+  tab: { title: 'title', url: 'url', isHovered: true, urlCount: 1 }
+}
+
+describe('Icon', () => {
+  it('should render correct components', () => {
+    const el = shallow(<Icon {...props} />)
+    expect(el.find(IconButton).length).toBe(1)
+    expect(el.find('img').length).toBe(1)
+  })
+})
+
+describe('TabTooltip', () => {
+  it('should render correct components', () => {
+    const el = shallow(
+      <TabTooltip.wrappedComponent.wrappedComponent {...props} />
+    )
+    expect(el.find(Tooltip).length).toBe(1)
+    expect(el.find(Tooltip).props().open).toBe(true)
+    const title = shallow(el.find(Tooltip).props().title)
+    expect(title.text()).toBe(props.tab.title + props.tab.url)
+  })
+
+  it('should render duplicated alert', () => {
+    const el = shallow(
+      <TabTooltip.wrappedComponent.wrappedComponent
+        {...props}
+        tab={{ ...props.tab, urlCount: 2 }}
+      />
+    )
+    const title = shallow(el.find(Tooltip).props().title)
+    expect(title.text()).toBe(
+      'There is duplicated tab!' + props.tab.title + props.tab.url
+    )
+  })
+
+  it('should render null', () => {
+    let el = shallow(
+      <TabTooltip.wrappedComponent.wrappedComponent {...props} faked />
+    )
+    expect(el.getElement()).toBe(null)
+    el = shallow(
+      <TabTooltip.wrappedComponent.wrappedComponent
+        {...props}
+        dragStore={{ dragging: true }}
+      />
+    )
+    expect(el.getElement()).toBe(null)
+    el = shallow(
+      <TabTooltip.wrappedComponent.wrappedComponent
+        {...props}
+        hoverStore={{ hovered: false }}
+      />
+    )
+    expect(el.getElement()).toBe(null)
+    el = shallow(
+      <TabTooltip.wrappedComponent.wrappedComponent
+        {...props}
+        tab={{ ...props.tab, isHovered: false }}
+      />
+    )
+    expect(el.getElement()).toBe(null)
+  })
+})
