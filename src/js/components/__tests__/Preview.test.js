af68f5343ef0bd17cd37d6ea032842a41e081ef9
xcv58 <i@xcv58.com>
2018-05-08 20:55:51 -0700

    Add test for Preview

---
+++ src/js/components/__tests__/Preview.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { shallow, it, describe, expect } from 'test'
+import Tab from 'components/Tab/Tab'
+import Preview from 'components/Preview'
+
+const sources = [{ id: 1 }, { id: 2 }]
+const props = {
+  tabStore: { sources }
+}
+
+describe('Preview', () => {
+  it('render correct components', () => {
+    const el = shallow(<Preview.wrappedComponent {...props} />)
+    expect(el.find('div').length).toBe(1)
+    expect(el.find(Tab).length).toBe(sources.length)
+    expect(
+      el
+        .find(Tab)
+        .first()
+        .props().faked
+    ).toBe(true)
+    expect(
+      el
+        .find(Tab)
+        .last()
+        .props().faked
+    ).toBe(true)
+  })
+})
