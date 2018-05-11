d4faf13a2088b5f2d8d7e4ae62ec7c330b1b22f0
xcv58 <i@xcv58.com>
2018-05-10 22:52:50 -0700

    fix: Add Toolbar icon to show shortcut hints

--- src/js/components/Toolbar/Toolbar.js
+++ src/js/components/Toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import { withTheme } from 'material-ui/styles'
 import Slide from 'material-ui/transitions/Slide'
 import SelectAll from './SelectAll'
 import Close from './Close'
@@ -8,17 +9,29 @@ import NewWindow from './NewWindow'
 import GroupAndSort from './GroupAndSort'
 import ToolbarSwitch from './ToolbarSwitch'
 import Settings from './Settings'
+import Help from './Help'
 
+@withTheme()
 @inject('userStore')
 @observer
 export default class Toolbar extends React.Component {
   render () {
+    const { theme } = this.props
     const { toolbarVisible } = this.props.userStore
     return (
       <Slide in={toolbarVisible} direction='up' style={{ display: 'flex' }}>
         <div>
           <ToolbarSwitch />
           <Settings />
+          <Help />
+          <div
+            style={{
+              margin: `0 ${theme.spacing.unit}px`,
+              backgroundColor: theme.palette.divider,
+              height: '89%',
+              width: 1
+            }}
+          />
           <GroupAndSort />
           <SelectAll />
           <InvertSelect />
