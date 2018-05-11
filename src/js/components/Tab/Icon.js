30490806d72604a976aa7b636dd54c1491febc59
xcv58 <i@xcv58.com>
2018-05-10 22:28:53 -0700

    Hover tab to show checkbox

--- src/js/components/Tab/Icon.js
+++ src/js/components/Tab/Icon.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import Checkbox from 'material-ui/Checkbox'
 import IconButton from 'material-ui/IconButton'
 
 const buttonWidth = '2.5rem'
@@ -8,7 +9,10 @@ const iconWidth = '1.5rem'
 @observer
 export default class Icon extends React.Component {
   render () {
-    const { focus, select, iconUrl } = this.props.tab
+    const { focus, select, iconUrl, isHovered, isSelected } = this.props.tab
+    if (isHovered) {
+      return <Checkbox color='primary' checked={isSelected} onChange={select} />
+    }
     return (
       <IconButton
         onClick={select}
--- src/js/components/Tab/__tests__/Icon.test.js
+++ src/js/components/Tab/__tests__/Icon.test.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { spy, shallow, expect, describe, it } from 'test'
+import Checkbox from 'material-ui/Checkbox'
 import IconButton from 'material-ui/IconButton'
 import Icon from 'components/Tab/Icon'
 
 const props = {
-  tab: { focus: spy(), select: spy(), iconUrl: 'url' }
+  tab: { focus: spy(), select: spy(), iconUrl: 'url', isHovered: false }
 }
 
 describe('Icon', () => {
-  it('should render correct components', () => {
+  it('should render IconButton if not hovered', () => {
     const el = shallow(<Icon {...props} />)
     expect(el.find(IconButton).length).toBe(1)
     expect(el.find('img').length).toBe(1)
+  })
+
+  it('should render Checkbox if hovered', () => {
+    const el = shallow(
+      <Icon {...props} tab={{ ...props.tab, isHovered: true }} />
+    )
+    expect(el.find(Checkbox).length).toBe(1)
+    expect(el.find(Checkbox).props().onChange).toBe(props.tab.select)
   })
 })
