bd54d69eb04444db31dd8703d6196e0bc69f76a5
xcv58 <i@xcv58.com>
2018-05-11 19:48:37 -0700

    Move SettingsDialog to App

--- src/js/components/Toolbar/index.js
+++ src/js/components/Toolbar/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import Toolbar from './Toolbar'
 import ToolbarIndicator from './ToolbarIndicator'
-import SettingsDialog from './SettingsDialog'
 
 @inject('userStore')
 @observer
@@ -28,7 +27,6 @@ export default class ToolbarWrapper extends React.Component {
       >
         {toolbarVisible && <Toolbar />}
         <ToolbarIndicator />
-        <SettingsDialog />
       </div>
     )
   }
