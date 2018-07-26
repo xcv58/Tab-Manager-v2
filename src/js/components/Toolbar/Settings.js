19c54214270fe74c20f6b3649fe4ee4bc3473907
Yihong <chenyihonglove@gmail.com>
2018-07-26 14:22:51 -0400

    yarn upgrade (#217)

--- src/js/components/Toolbar/Settings.js
+++ src/js/components/Toolbar/Settings.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-import Settings from '@material-ui/icons/Settings'
+import Settings from '@material-ui/icons/SettingsSharp'
 import IconButton from '@material-ui/core/IconButton'
 import Tooltip from '@material-ui/core/Tooltip'
 import { TOOLTIP_DELAY } from 'libs'
