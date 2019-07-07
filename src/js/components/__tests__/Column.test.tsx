de061e0fc75802bdbbf5d64a74f64e06993a03c9
xcv58 <i@xcv58.com>
2019-07-07 16:59:22 -0700

    fix test

--- src/js/components/__tests__/Column.test.tsx
+++ src/js/components/__tests__/Column.test.tsx
@@ -5,6 +5,7 @@ import { shallow } from 'enzyme'
 import Column from 'components/Column'
 import Window from 'components/Window'
 import { createMuiTheme } from '@material-ui/core/styles'
+import FlipMove from 'react-flip-move'
 
 const theme = createMuiTheme()
 const windows = [{ id: 1 }, { id: 2 }]
@@ -25,22 +26,23 @@ const props = {
 describe('Column', () => {
   it('render correct components', () => {
     const el = shallow(<Column {...props} />)
-    expect(el.find('div').length).toBe(1)
+    expect(el.find(FlipMove).length).toBe(1)
+    expect(el.find('div').length).toBe(2)
     expect(el.find(Window).length).toBe(2)
   })
 
   it('render correct margin based on left & right', () => {
     let el = shallow(<Column {...props} left />)
-    expect(el.find('div').props().style.marginLeft).toBe('auto')
-    expect(el.find('div').props().style.marginRight).toBeUndefined()
+    expect(el.find(FlipMove).props().style.marginLeft).toBe('auto')
+    expect(el.find(FlipMove).props().style.marginRight).toBeUndefined()
     el = shallow(<Column {...props} right />)
-    expect(el.find('div').props().style.marginLeft).toBeUndefined()
-    expect(el.find('div').props().style.marginRight).toBe('auto')
+    expect(el.find(FlipMove).props().style.marginLeft).toBeUndefined()
+    expect(el.find(FlipMove).props().style.marginRight).toBe('auto')
     el = shallow(<Column {...props} left right />)
-    expect(el.find('div').props().style.marginLeft).toBe('auto')
-    expect(el.find('div').props().style.marginRight).toBe('auto')
+    expect(el.find(FlipMove).props().style.marginLeft).toBe('auto')
+    expect(el.find(FlipMove).props().style.marginRight).toBe('auto')
     el = shallow(<Column {...props} />)
-    expect(el.find('div').props().style.marginLeft).toBeUndefined()
-    expect(el.find('div').props().style.marginRight).toBeUndefined()
+    expect(el.find(FlipMove).props().style.marginLeft).toBeUndefined()
+    expect(el.find(FlipMove).props().style.marginRight).toBeUndefined()
   })
 })
