06b349da74d565e260a2687506dc0423216fcd69
xcv58 <i@xcv58.com>
2020-03-07 14:21:59 -0800

    Tune close button style

--- src/js/components/CloseButton.tsx
+++ src/js/components/CloseButton.tsx
@@ -4,8 +4,11 @@ export default props => {
   const { onClick, disabled } = props
   return (
     <button
-      {...{ onClick, disabled }}
-      className='bg-transparent hover:text-red-700 text-red-300 border-0 py-3 px-4'
+      {...{
+        onClick,
+        disabled
+      }}
+      className='inline-flex items-center justify-center w-8 h-8 p-4 m-2 text-red-300 bg-transparent rounded-full hover:text-red-500 hover:bg-red-100 focus:outline-none focus:shadow-outline active:bg-red-300 active:text-red-700'
     >
       x
     </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   theme: {
     extend: {}
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
   plugins: []
 }
