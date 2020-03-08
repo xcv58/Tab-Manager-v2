09b1a199296ae44ed74ed4cf729ae2a101d7934c
xcv58 <i@xcv58.com>
2020-03-08 12:46:34 -0700

    Use tailwind for Tab Icon

--- src/js/components/Tab/Icon.tsx
+++ src/js/components/Tab/Icon.tsx
@@ -2,47 +2,21 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import Checkbox from '@material-ui/core/Checkbox'
 import IconButton from '@material-ui/core/IconButton'
-import { makeStyles } from '@material-ui/core'
-
-const buttonWidth = '39px'
-const iconWidth = '1.5rem'
-
-const useStyles = makeStyles(() => ({
-  root: {
-    '&:hover $icon': {
-      display: 'none'
-    },
-    '&:hover $checkbox': {
-      display: 'flex'
-    }
-  },
-  icon: {
-    width: buttonWidth,
-    height: buttonWidth
-  },
-  img: {
-    width: iconWidth,
-    height: iconWidth
-  },
-  checkbox: {
-    display: 'none'
-  }
-}))
 
 export default observer(props => {
-  const classes = useStyles(props)
   const { focus, select, iconUrl, isSelected } = props.tab
   return (
-    <div className={classes.root}>
-      <IconButton className={classes.icon} onClick={select} onFocus={focus}>
-        <img className={classes.img} src={iconUrl} />
+    <div className='group'>
+      <IconButton
+        className='group-hover:hidden'
+        onClick={select}
+        onFocus={focus}
+      >
+        <img className='w-6 h-6' src={iconUrl} />
       </IconButton>
-      <Checkbox
-        className={classes.checkbox}
-        color='primary'
-        checked={isSelected}
-        onChange={select}
-      />
+      <div className='hidden group-hover:block'>
+        <Checkbox color='primary' checked={isSelected} onChange={select} />
+      </div>
     </div>
   )
 })
