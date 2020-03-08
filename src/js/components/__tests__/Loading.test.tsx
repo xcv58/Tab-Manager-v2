e6f50f339908561e041a8891fa7f9dea588d9a35
xcv58 <i@xcv58.com>
2020-03-08 16:21:09 -0700

    Fix test

--- src/js/components/__tests__/Loading.test.tsx
+++ src/js/components/__tests__/Loading.test.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import Loading from 'components/Loading'
+import { ThemeContext } from 'components/ThemeContext'
 
 describe('Loading should', () => {
   it('render divs', () => {
-    const el = shallow(<Loading />)
+    const el = mount(<Loading />)
     expect(el.find('div').length).toBe(10)
     const spinner = el.find({ id: 'spinner' })
     expect(spinner.length).toBe(1)
-    const la = spinner.find({ className: 'la-ball-spin la-dark la-3x' })
+    const la = spinner.find({ className: 'la-ball-spin la-3x la-dark' })
+    expect(la.length).toBe(1)
+    expect(la.find('div').length).toBe(9)
+    expect(el.text()).toBe('')
+  })
+
+  it('render divs in dark mode', () => {
+    const el = mount(
+      <ThemeContext.Provider value>
+        <Loading />
+      </ThemeContext.Provider>
+    )
+    expect(el.find('div').length).toBe(10)
+    const spinner = el.find({ id: 'spinner' })
+    expect(spinner.length).toBe(1)
+    const la = el.find({ className: 'la-ball-spin la-3x' })
     expect(la.length).toBe(1)
     expect(la.find('div').length).toBe(9)
     expect(el.text()).toBe('')
