ff11600283022a2a176a83d96c11f1ba0f4608df
Yihong <i@xcv58.com>
2020-03-22 16:16:25 -0700

    feat: Use React Concurrent mode (#311)

--- src/js/popup.tsx
+++ src/js/popup.tsx
@@ -1,15 +1,17 @@
 import App from 'components/App'
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { StoreContext, store } from 'components/StoreContext'
 import '../css/popup.css'
 
 const init = () => {
-  render(
+  const container = window.document.getElementById('app-container')
+  container.removeChild(container.firstChild)
+
+  ReactDOM.createRoot(container).render(
     <StoreContext.Provider value={store}>
       <App />
-    </StoreContext.Provider>,
-    window.document.getElementById('app-container')
+    </StoreContext.Provider>
   )
   if (process.env.NODE_ENV !== 'production') {
     require('react-axe')(React, require('react-dom'), 1000)
