101868d7f2c47f11244273828a962ca6bf8dd878
xcv58 <i@xcv58.com>
2020-03-23 19:42:15 -0700

    Revert "feat: Use React Concurrent mode (#311)"
    This reverts commit ff11600283022a2a176a83d96c11f1ba0f4608df.

--- src/js/popup.tsx
+++ src/js/popup.tsx
@@ -1,17 +1,15 @@
 import App from 'components/App'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { StoreContext, store } from 'components/StoreContext'
 import '../css/popup.css'
 
 const init = () => {
-  const container = window.document.getElementById('app-container')
-  container.removeChild(container.firstChild)
-
-  ReactDOM.createRoot(container).render(
+  render(
     <StoreContext.Provider value={store}>
       <App />
-    </StoreContext.Provider>
+    </StoreContext.Provider>,
+    window.document.getElementById('app-container')
   )
   if (process.env.NODE_ENV !== 'production') {
     require('react-axe')(React, require('react-dom'), 1000)
