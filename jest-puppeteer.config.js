868ad7a07bfcc23d78a305cac186d9327250e6f2
xcv58 <i@xcv58.com>
2020-04-06 11:41:21 -0700

    clean up

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,6 +5,7 @@ const extensionPath = path.join(__dirname, 'build')
 module.exports = {
   launch: {
     // headless: process.env.HEADLESS !== 'false',
+    // Must set headless to false. Otherwise, the extension wouldn't load.
     headless: false,
     args: [
       `--disable-extensions-except=${extensionPath}`,
