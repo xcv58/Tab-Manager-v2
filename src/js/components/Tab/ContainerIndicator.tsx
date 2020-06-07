7d51c9089fb4bd9e4c4dc01fae000b951d1f132b
xcv58 <i@xcv58.com>
2020-06-07 14:47:26 -0700

    Add test for ContainerIndicator

--- src/js/components/Tab/ContainerIndicator.tsx
+++ src/js/components/Tab/ContainerIndicator.tsx
@@ -1,31 +1,9 @@
-import React from 'react'
 import { observer } from 'mobx-react-lite'
-import { useStore } from 'components/hooks/useStore'
 
 let Dump: any = () => null
 
 if (process.env.TARGET_BROWSER === 'firefox') {
-  const ContainerIndicator = (props) => {
-    const { cookieStoreId } = props
-    const { containerStore } = useStore()
-    const container = containerStore.getContainer(cookieStoreId)
-    if (!container) {
-      return null
-    }
-    const { colorCode } = container
-    return (
-      <hr
-        className='absolute border'
-        style={{
-          width: 'calc(100% - 24px)',
-          left: 12,
-          bottom: 2,
-          borderColor: colorCode
-        }}
-      />
-    )
-  }
-  Dump = observer(ContainerIndicator)
+  Dump = observer(require('./_ContainerIndicator').default)
 }
 
 export default Dump
