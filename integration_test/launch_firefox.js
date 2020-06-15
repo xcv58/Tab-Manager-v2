16bd84aedf72b8fcd89bd2023303ea9577633a87
xcv58 <i@xcv58.com>
2020-06-14 18:08:42 -0700

    Clean up

--- integration_test/launch_firefox.js
+++ integration_test/launch_firefox.js
@@ -3,7 +3,6 @@ const path = require('path')
 const getPort = require('get-port')
 const webExt = require('web-ext').default
 const puppeteer = require('puppeteer')
-console.log(webExt)
 
 const sourceDir = path.join(__dirname, '../build_firefox')
 const firefox = path.join(
@@ -17,7 +16,6 @@ const openPage = async (browser, url) => {
   return page
 }
 
-console.log({ sourceDir, firefox })
 ;(async () => {
   const CDPPort = await getPort()
   const args = [`--remote-debugger=localhost:${CDPPort}`]
@@ -31,21 +29,16 @@ console.log({ sourceDir, firefox })
       shouldExitProgram: false
     }
   )
-  // The command has finished. Each command resolves its
-  // promise with a different value.
-  console.log(extensionRunner)
+  // The command has finished. Each command resolves its promise with a different value.
   // You can do a few things like:
-  // extensionRunner.reloadAllExtensions();
+  extensionRunner.reloadAllExtensions()
   // extensionRunner.exit();
 
   // Needed because `webExt.cmd.run` returns before the DevTools agent starts running.
   // Alternative would be to wrap the call to puppeteer.connect() with some custom retry logic
-  console.log('before sleep')
   childProcess.execSync('sleep 2')
-  console.log('after sleep')
 
   const browserURL = `http://localhost:${CDPPort}`
-  // const browser = await puppeteer.launch({ headless: false, product: "firefox" })
   const browser = await puppeteer.connect({ browserURL })
   let page = await openPage(browser, 'https://google.com')
   await page.keyboard.press('Tab')
@@ -53,6 +46,5 @@ console.log({ sourceDir, firefox })
   await page.keyboard.press('o')
   await page.keyboard.up('Control')
   // When it open new tab, the old one would execute until the tab is active
-  console.log('new page')
   page = await openPage(browser, 'https://google.com')
 })()
