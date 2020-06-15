e4dcceef9460dac1542fb410f874034a7586a028
xcv58 <i@xcv58.com>
2020-06-14 17:58:23 -0700

    clean up

--- integration_test/puppeteer.config.js
+++ integration_test/puppeteer.config.js
@@ -1,12 +1,10 @@
 const path = require('path')
 
-// const extensionPath = path.join(__dirname, '../build_chrome')
-const extensionPath = path.join(__dirname, '../build_firefox')
+const extensionPath = path.join(__dirname, '../build_chrome')
 
 const launch = {
   // Chrome Headless doesn't support extensions https://github.com/puppeteer/puppeteer/issues/659
   headless: false,
-  product: 'firefox',
   args: [
     `--disable-extensions-except=${extensionPath}`,
     `--load-extension=${extensionPath}`
