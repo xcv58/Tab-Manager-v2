2ffbfe73c5d832517e49ec85ccb1b57337404367
xcv58 <i@xcv58.com>
2020-06-24 22:48:31 -0700

    fix: The drop indicator is invisible on the bottom of a window

--- src/js/components/DropIndicator.tsx
+++ src/js/components/DropIndicator.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default () => (
   <hr
-    className='z-10 border-t-0 border-b border-red-700'
+    className='relative z-10 border-t-0 border-b-2 border-red-700'
     style={{
-      marginTop: -1
+      marginTop: -2
     }}
   />
 )
