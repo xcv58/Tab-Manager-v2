252ea74acc5b2f797c94392ffdfdb818402cfa41
Deepak Agrawal <dagrawal88@gmail.com>
2020-08-02 11:45:39 -0700

    Adding Close Button to Search Results (#594)
    Co-authored-by: Deepak Agrawal <dagrawal@twitter.com>

--- src/js/components/Tab/ViewOnlyTab.tsx
+++ src/js/components/Tab/ViewOnlyTab.tsx
@@ -6,10 +6,19 @@ import TabContent from 'components/Tab/TabContent'
 import { TabProps } from 'components/types'
 import PIN from './Pin'
 import ContainerIndicator from './ContainerIndicator'
+import CloseButton from 'components/CloseButton'
 
 export default observer((props: TabProps) => {
   const { tab } = props
   const pin = tab.pinned && PIN
+
+  const onRemove = (event: React.SyntheticEvent) => {
+    event.stopPropagation()
+    const { removing, remove } = tab
+    if (!removing) {
+      remove()
+    }
+  }
 
   return (
     <div tabIndex={-1} className='relative flex w-full'>
@@ -18,6 +27,7 @@ export default observer((props: TabProps) => {
       <TabContent tab={tab} faked />
       <TabTools tab={tab} faked />
       <ContainerIndicator cookieStoreId={tab.cookieStoreId} />
+      <CloseButton onClick={onRemove} disabled={tab.removing} />
     </div>
   )
 })
