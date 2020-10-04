2d5613ec6d26c8ccbb4d91576f7ac33851323174
xcv58 <i@xcv58.com>
2020-10-03 20:01:35 -0700

    Upgrade postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -95,8 +95,10 @@ const options = {
           {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',
-              plugins: [require('tailwindcss'), require('autoprefixer')]
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [require('tailwindcss'), require('autoprefixer')]
+              }
             }
           }
         ],
