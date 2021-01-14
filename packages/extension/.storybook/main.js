54d7606757f967c386e0252199f24da19abf94fa
xcv58 <i@xcv58.com>
2021-01-13 21:28:36 -0800

    Add Storybook addons: storybook-addon-performance and @storybook/addon-a11y

--- packages/extension/.storybook/main.js
+++ packages/extension/.storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    'storybook-addon-performance/register',
+  ],
 }
--- packages/extension/.storybook/preview.js
+++ packages/extension/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { withPerformance } from 'storybook-addon-performance'
+
+addDecorator(withPerformance)
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
