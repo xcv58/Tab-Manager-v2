6d71a25a93763addd38762114b360f4fe0591302
xcv58 <i@xcv58.com>
2021-03-01 12:57:08 -0500

    Use Playwright for integration test (#1003)

--- packages/integration_test/test/extension.test.ts
+++ packages/integration_test/test/extension.test.ts
@@ -1,20 +1,46 @@
+import { Page, ChromiumBrowserContext } from 'playwright'
+import manifest from '../../extension/src/manifest.json'
 import {
-  manifest,
   TAB_QUERY,
-  ALL,
-  EACH,
-  CLOSE_PAGES,
   URLS,
   isExtensionURL,
+  CLOSE_PAGES,
+  initBrowserWithExtension,
+  openPages,
 } from '../util'
+import expect from 'expect'
+
+let page: Page
+let browserContext: ChromiumBrowserContext
+let extensionURL: string
 
 describe('The Extension page should', () => {
-  beforeAll(ALL)
-  beforeEach(EACH)
+  beforeAll(async () => {
+    const init = await initBrowserWithExtension()
+    browserContext = init.browserContext
+    extensionURL = init.extensionURL
+    page = browserContext.pages()[0]
+  })
 
-  afterEach(CLOSE_PAGES)
+  afterAll(async () => {
+    await browserContext?.close()
+    browserContext = null
+    page = null
+    extensionURL = ''
+  })
+
+  beforeEach(async () => {
+    console.log({ extensionURL })
+    await page.waitForTimeout(1000)
+    await page.bringToFront()
+  })
+
+  afterEach(async () => {
+    await CLOSE_PAGES(browserContext)
+  })
 
   it('have title ends with the extension name', async () => {
+    await page.goto(extensionURL)
     await expect(page.title()).resolves.toMatch(manifest.name)
   })
 
@@ -25,15 +51,15 @@ describe('The Extension page should', () => {
     expect(tabs).toHaveLength(1)
 
     const N = 10
-    await Promise.all(
-      [...Array(10)].map(async () => {
-        const newPage = await browser.newPage()
-        await newPage.goto('https://google.com')
-      })
+
+    await openPages(
+      browserContext,
+      [...Array(10)].map((_) => 'https://google.com')
     )
+    await page.bringToFront()
     tabs = await page.$$(TAB_QUERY)
     expect(tabs).toHaveLength(N + 1)
-    const pages = await browser.pages()
+    const pages = await browserContext.pages()
     expect(pages).toHaveLength(N + 1)
     await page.bringToFront()
   })
@@ -41,26 +67,22 @@ describe('The Extension page should', () => {
   it('sort the tabs', async () => {
     const wins = await page.$$('.shadow-2xl,.shadow-sm')
     expect(wins).toHaveLength(1)
-    let tabs = await page.$$(TAB_QUERY)
+    const tabs = await page.$$(TAB_QUERY)
     expect(tabs).toHaveLength(1)
-    await Promise.all(
-      URLS.map(async (url) => {
-        const newPage = await browser.newPage()
-        await newPage.goto(url)
-      })
-    )
+    await openPages(browserContext, URLS)
     await page.bringToFront()
-    tabs = await page.$$eval(TAB_QUERY, (nodes) =>
+
+    let tabURLs = await page.$$eval(TAB_QUERY, (nodes) =>
       nodes.map((node) => node.querySelector('.text-xs').innerText)
     )
-    expect(tabs).toHaveLength(URLS.length + 1)
-    expect(tabs.filter((tab) => !isExtensionURL(tab))).toEqual([
+    expect(tabURLs).toHaveLength(URLS.length + 1)
+    expect(tabURLs.filter((tab) => !isExtensionURL(tab))).toEqual([
       'https://www.google.com/',
       'https://github.com/',
       'https://www.google.com/',
     ])
-    let pages = await browser.pages()
-    let urls = await Promise.all(pages.map(async (page) => await page.url()))
+    const pages = await browserContext.pages()
+    const urls = await Promise.all(pages.map(async (page) => await page.url()))
     expect(urls.filter((x) => !isExtensionURL(x))).toEqual([
       'https://www.google.com/',
       'https://github.com/',
@@ -71,11 +93,11 @@ describe('The Extension page should', () => {
     await sortTabsButton.click()
     await page.waitForTimeout(100)
 
-    tabs = await page.$$eval(TAB_QUERY, (nodes) =>
+    tabURLs = await page.$$eval(TAB_QUERY, (nodes) =>
       nodes.map((node) => node.querySelector('.text-xs').innerText)
     )
-    expect(tabs).toHaveLength(URLS.length + 1)
-    expect(tabs.filter((x) => !isExtensionURL(x))).toEqual([
+    expect(tabURLs).toHaveLength(URLS.length + 1)
+    expect(tabURLs.filter((x) => !isExtensionURL(x))).toEqual([
       'https://github.com/',
       'https://www.google.com/',
       'https://www.google.com/',
@@ -83,16 +105,11 @@ describe('The Extension page should', () => {
   })
 
   it('close tab when click close button', async () => {
-    await Promise.all(
-      URLS.map(async (url) => {
-        const newPage = await browser.newPage()
-        await newPage.goto(url)
-      })
-    )
+    await openPages(browserContext, URLS)
     await page.bringToFront()
     let tabs = await page.$$(TAB_QUERY)
 
-    const pages = await browser.pages()
+    const pages = await browserContext.pages()
     expect(tabs.length).toBe(pages.length)
     let buttons = await tabs[tabs.length - 1].$$('button')
     expect(buttons).toHaveLength(3)
---
+++ packages/integration_test/util.ts
@@ -0,0 +1,85 @@
+import { chromium, ChromiumBrowserContext, Page } from 'playwright'
+import { join } from 'path'
+
+export const EXTENSION_PATH = join(__dirname, '../../build_chrome')
+
+export const TAB_QUERY = 'div[draggable="true"] div[tabindex="-1"]'
+
+export const URLS = [
+  'https://www.google.com/',
+  'https://github.com/',
+  'https://www.google.com/',
+]
+
+export const isExtensionURL = (url: string) =>
+  url.startsWith('chrome-extension://')
+
+export const CLOSE_PAGES = async (browserContext: ChromiumBrowserContext) => {
+  const pages = (await browserContext?.pages()) || []
+  for (const page of pages) {
+    const url = await page.url()
+    if (!isExtensionURL(url)) {
+      await page.close()
+    }
+  }
+}
+
+export const initBrowserWithExtension = async () => {
+  const userDataDir = `/tmp/test-user-data-${Math.random()}`
+  let extensionURL: string
+  const browserContext = (await chromium.launchPersistentContext(userDataDir, {
+    headless: false,
+    args: [
+      // Follow suggestions on https://playwright.dev/docs/ci#docker
+      '--disable-dev-shm-usage',
+      '--ipc=host',
+      `--disable-extensions-except=${EXTENSION_PATH}`,
+      `--load-extension=${EXTENSION_PATH}`,
+    ],
+  })) as ChromiumBrowserContext
+
+  /**
+   * The background page is useful to retrieve the extension id so that we
+   * could programatically open the extension page.
+   *
+   * There is uncertain timing of backgroundPages. Sometimes the
+   * `browserContext.backgroundPages()` will return empty at the beginning,
+   * so we have to rely on the `browserContext.on('backgroundpage')` to get
+   * the background page. But sometimes the 'backgroundpage' would never be
+   * triggered and the `browserContext.backgroundPages()` would give an array
+   * with the existing background page.
+   */
+  const setExtensionURL = (backgroundPage: Page) => {
+    const url = backgroundPage.url()
+    const [, , extensionId] = url.split('/')
+    extensionURL = `chrome-extension://${extensionId}/popup.html?not_popup=1`
+    console.log('browserContext.on backgroundpage', { extensionURL })
+  }
+
+  browserContext.on('backgroundpage', setExtensionURL)
+  const backgroundPages = browserContext.backgroundPages()
+  if (backgroundPages.length) {
+    setExtensionURL(backgroundPages[0])
+  }
+  const page = await browserContext.newPage()
+  await page.bringToFront()
+  for (const x in [...Array(100)]) {
+    if (extensionURL || x) {
+      break
+    }
+    await page.waitForTimeout(100)
+  }
+  return { browserContext, extensionURL }
+}
+
+export const openPages = async (
+  browserContext: ChromiumBrowserContext,
+  urls: string[]
+) => {
+  return await Promise.all(
+    urls.map(async (url) => {
+      const newPage = await browserContext.newPage()
+      return await newPage.goto(url)
+    })
+  )
+}
