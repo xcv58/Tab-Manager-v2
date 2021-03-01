e8ed3d64c407f1e38d91cd1e9b53a8afe3af3a9e
xcv58 <i@xcv58.com>
2021-02-28 20:25:47 -0800

    Remove expect-puppeteer

--- packages/integration_test/jest.config.js
+++ packages/integration_test/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-playwright-preset',
   testRegex: './*\\.test\\.ts$',
-  setupFilesAfterEnv: ['expect-puppeteer', './setup.js'],
+  setupFilesAfterEnv: ['./setup.js'],
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
   },
