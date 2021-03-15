b1ae71225e1bf720c9ddfc95fc88d5fe65e4c977
xcv58 <i@xcv58.com>
2021-03-15 00:25:42 -0700

    failureThreshold: 0.05

--- packages/integration_test/test/extension.test.ts
+++ packages/integration_test/test/extension.test.ts
@@ -17,7 +17,7 @@ expect.extend({ toMatchImageSnapshot })
 
 const matchImageSnapshotOptions: MatchImageSnapshotOptions = {
   updatePassedSnapshot: true,
-  failureThreshold: 0.1,
+  failureThreshold: 0.05,
   failureThresholdType: 'percent',
 }
 
