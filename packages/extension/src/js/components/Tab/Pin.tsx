8924fe18805bad80b3f8ad997cc1b98151c35e9f
xcv58 <i@xcv58.com>
2023-03-15 22:58:34 -0400

    fix: The pin indicator position

--- packages/extension/src/js/components/Tab/Pin.tsx
+++ packages/extension/src/js/components/Tab/Pin.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PIN = (
-  <div className="z-auto w-0 h-0 text-xs origin-bottom-right transform -rotate-90 translate-y-4 pointer-events-none">
+  <div className="z-auto w-0 h-0 text-xs origin-bottom-right transform -rotate-90 pointer-events-none">
     📌
   </div>
 )
