3eedcdf6db698dc98e015547da3bd7c0697a8ae1
xcv58 <i@xcv58.com>
2023-03-19 15:49:09 -0400

    fix: Do not wrap the window title

--- packages/extension/src/js/components/Window/Title.tsx
+++ packages/extension/src/js/components/Window/Title.tsx
@@ -38,7 +38,7 @@ export default observer((props: WinProps & { className: string }) => {
       ref={nodeRef}
       className={classNames(
         'flex justify-between items-center font-bold border-0',
-        className
+        className,
       )}
     >
       <SelectAll {...props} />
@@ -46,7 +46,7 @@ export default observer((props: WinProps & { className: string }) => {
         onClick={activate}
         className="flex-auto overflow-hidden text-base text-left rounded-sm"
       >
-        <h5 className="flex-auto text-2xl">
+        <h5 className="flex-auto text-2xl whitespace-nowrap">
           {text} {!hide && invisibleIndicator}
         </h5>
       </button>
