388540f1025edf37b158084c45ff4f1eca5e170c
xcv58 <i@xcv58.com>
2023-12-03 22:32:31 -0500

    Upgrade to storybook 7 (#2464)

---
+++ packages/storybook/.storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    options: {
+      isToolshown: true,
+      storySort: {
+        order: ['Introduction', 'Changelog', 'UI Components'],
+      },
+    },
+  },
+}
+
+export default preview
