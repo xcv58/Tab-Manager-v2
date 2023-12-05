18a7e52f2ec1897ed6f63de17bc0c039c93e2bfe
xcv58 <i@xcv58.com>
2023-12-04 23:50:00 -0500

    Tune color

--- packages/extension/src/js/components/Tab/Icon.tsx
+++ packages/extension/src/js/components/Tab/Icon.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import classNames from 'classnames'
 import { observer } from 'mobx-react-lite'
 import { Checkbox } from '@material-tailwind/react'
-import { IconButton } from '@material-tailwind/react'
 import { useStore } from 'components/hooks/useStore'
 import { TabProps } from 'components/types'
-
-const ARIA_LABEL = 'Toggle select'
 
 export const Icon = observer((props: TabProps) => {
   const { userStore } = useStore()
   const { select, iconUrl, isSelected, bulkSelect } = props.tab
   const checkbox = (
     <Checkbox
+      color="indigo"
       checked={isSelected}
       onClick={(e) => {
         if (process.env.TARGET_BROWSER === 'firefox') {
@@ -34,7 +32,7 @@ export const Icon = observer((props: TabProps) => {
   return (
     <div className="flex justify-center w-12 group">
       <div
-        className={classNames({
+        className={classNames('bg-blue-gray-100', {
           hidden: isSelected,
           'group-hover:hidden': !isSelected,
         })}
@@ -59,14 +57,8 @@ export default observer((props: TabProps) => {
     return <Icon {...props} />
   }
   return (
-    <div>
-      <IconButton
-        disabled
-        aria-label={ARIA_LABEL}
-        className="focus:outline-none focus:ring"
-      >
-        <img className="w-6 h-6" src={iconUrl} />
-      </IconButton>
+    <div className="flex items-center justify-center w-10 group">
+      <img className="w-6 h-6" src={iconUrl} />
     </div>
   )
 })
