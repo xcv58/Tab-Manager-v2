69c11b95faea2e0c732f0b51d6079768996e3f58
xcv58 <i@xcv58.com>
2026-01-31 14:06:39 -0500

    Regenerate snapshots

--- generate_snapshots.sh
+++ generate_snapshots.sh
@@ -10,9 +10,13 @@ docker run --rm --ipc=host \
   -v /work/packages/integration_test/node_modules \
   -w /work \
   -e CI=true \
+  -e TURBO_TEAM="$TURBO_TEAM" \
+  -e TURBO_TOKEN="$TURBO_TOKEN" \
   -e DEBUG=pw:browser* \
-  mcr.microsoft.com/playwright:v1.58.0-jammy \
+  mcr.microsoft.com/playwright:v1.58.0-noble \
   /bin/bash -c "
+    echo 'Installing xvfb...'
+    apt-get update && apt-get install -y xvfb
     echo 'Installing pnpm...'
     npm install -g pnpm@9.15.9
     echo 'Installing dependencies...'
@@ -20,6 +24,6 @@ docker run --rm --ipc=host \
     echo 'Running update-snapshots...'
     # Directly reuse the test script from packages/integration_test/package.json
     # CI=true ensures it runs with xvfb-run
-    pnpm --filter integration-test run test -- --update-snapshots
+    xvfb-run --auto-servernum --server-args='-screen 0 1280x960x24' pnpm --filter integration-test run test --update-snapshots
     echo 'Finished.'
   "
